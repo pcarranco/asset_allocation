e3b44e1171a9d8df9f3b2582617925f2394b0a7a
Pedro Carranco <carranco173@gmail.com>
2021-05-28 23:29:51 -0500

    Se agregaron las funciones para realizar primera simulacion

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Crear portafolios de renta fija y renta variabled
 
+from numpy.compat.py3k import contextlib_nullcontext
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.cbook as cbook
+import matplotlib.ticker as mtick
 
-# ________________________________ calculo de variaciones ____________________________
+# ________________________________ Cálculo de variaciones ____________________________
 
 def variaciones():
     df = pd.read_csv('data.csv',header=0,index_col=0, parse_dates=['Fecha'])
@@ -15,77 +17,62 @@ def variaciones():
     df.dropna(inplace=True)
     return df
 
-# _________________________ funciones de visualizacion de datos _____________________
-
-def graficar_rendimiento(var):
-    
-    var = variaciones()
-    rend = var.cumprod()
-    
-    
-    plt.plot(rend.index, rend.values)
-    plt.legend()
-    plt.show()
-
-# ________________________ Funciones de portafilios ________________________________
+# ________________________ Funciones cálculo métricas de portafilios __________________
 
 def rendimiento_port(vars, wgts):
     
-    rend=  vars.prod()
+    rend=  (vars.mean()**12)-1
     
     rend = rend*wgts
     
-    return rend.sum()
+    return rend.sum() * 100
 
 def volatilidad_port(vars, wgts):
     
-    covs =vars.cov()
+    covs =np.array(vars.cov())
+    
+    arr = np.array(wgts)
+    
+    arr = arr*arr.reshape(len(wgts),1)
+    
+    var = arr * covs
+    
+    return np.sqrt(np.sum(var))*np.sqrt(12) * 100
+
+# _________________________ Funciones de visualización de datos _____________________
+
+def graficar_portafolios(rends, vols):
+
+    plt.plot(vols, rends)
+    plt.show()
+
+# ____________________________ Simulaciones __________________________________
+
+def simulacion_portafilio_2_activos(nportafolios):
+    
+    var = variaciones()
+    var.drop(['RF 0-1', 'RF 1-5'],axis=1,inplace=True)
+
+    wgtsList = []
     n = 0
-    port_vol = 0
+    salto = 1/nportafolios
+    
+    for _ in range(nportafolios):
+       w = [round(1-n,2),round(n,2)]
+       wgtsList.append(w)
+       n+=salto
+    
+    rends = []
+    vols = []
+    
+    for comb in wgtsList:
+        rport = rendimiento_port(var,comb) 
+        vport = volatilidad_port(var, comb)
 
-    
-    for col in covs:
-        indiv = (np.power(covs[col][n], 2) * np.power(wgts[n], 2))
-        port_vol = port_vol + indiv
-        n+=1
-    
-    r = covs.iloc[0][1] * wgts[0] * wgts[1] * 2
-
-    port_vol = port_vol + r
-
-    return np.sqrt(port_vol)
-
-# _______________________ Sin clasificar __________________________________________
-
-def prueba():
-    var = variaciones()
-    
-    rf = [var['RF 0-1'].std(), var['RF 0-1'].prod()]
-    rv = [var['RV'].std(), var['RV'].prod()]
-    
-    i = 0
-    port_list = [] 
-    port_max_rend = []
-    max_rend = 0
-    
-    for _ in range(10):
-    
-        port = [rf[1]*i, rv[1]*(1-i),(rf[1]*i) + (rv[1]*(1-i))]
-        i+=.1
+        rends.append(rport)
+        vols.append(vport)
         
-        if port[2] > max_rend:
-            port_max_rend = port
-            max_rend = port[2]
-        
-         
-        port_list.append(port)
+    graficar_portafolios(rends, vols)
     
-    
-    print(max_rend, port_max_rend)
-
-
-
 if __name__ == '__main__':
-    var = variaciones()
-    var.drop(['RF 1-5', 'RF 5-10'], axis= 1, inplace=True)
-    print(volatilidad_port(var,[0.1, 0.9]))
+    simulacion_portafilio_2_activos(100)
