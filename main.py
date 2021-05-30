a8cfea31face8848647eff4de2b4fee06622413b
Pedro Carranco <carranco173@gmail.com>
2021-05-29 21:28:15 -0500

    Se agregaron nuevas funciones de visualización y más bases de datos

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 # Crear portafolios de renta fija y renta variabled
 
-from numpy.compat.py3k import contextlib_nullcontext
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.cbook as cbook
-import matplotlib.ticker as mtick
 
 # ________________________________ Cálculo de variaciones ____________________________
 
-def variaciones():
-    df = pd.read_csv('data.csv',header=0,index_col=0, parse_dates=['Fecha'])
-    for col in df:
-        df[col] = df[col]/df[col].shift(1)
-    
+def variaciones_csv(filepath):
+    df = pd.read_csv(filepath,header=0,index_col=0, parse_dates=[0])
+    df = df/df.shift(1)
     df.dropna(inplace=True)
+    
     return df
 
 # ________________________ Funciones cálculo métricas de portafilios __________________
@@ -58,13 +54,47 @@ def graficar_3_curvas(rends, vols):
     plt.ylabel = 'Rendimiento'
     plt.legend()
     plt.show()
+    
+def graficar_retornos(filepath, start=None, end=None, series=[]):
+
+    var = variaciones_csv(filepath)
+    
+    if not series:
+        fig = var.loc[start:end,:]
+    else:
+        fig = var.loc[start:end, series]
+    fig = fig.cumprod() * 100
+    
+    fig.plot()
+    plt.show()
+    
+def graficar_retorno_volatilidad(filepath, start=None, end=None,series=[]):
+
+    var = variaciones_csv(filepath)
+    
+    if not series:
+        fig = var.loc[start:end,:]
+    else:
+        fig = var.loc[start:end, series]
+    
+    rend = ((fig.mean() ** 12) -1) * 100
+    vols = fig.std() * np.sqrt(12) * 100
+    
+
+    for i in range(len(rend)):
+        plt.scatter(vols[i],rend[i], label = rend.index[i], alpha=0.8, edgecolors='none')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
 # ____________________________ Simulaciones __________________________________
 
-def simulacion_portafilio_2_activos(nportafolios):
+def simulacion_portafilio_2_activos(filepath, series, start=None, end=None, nportafolios=20):
     
-    var = variaciones()
-    var.drop(['RF 0-1', 'RF 1-5'],axis=1,inplace=True)
-
+    var = variaciones_csv(filepath)
+    
+    var = var.loc[start:end, series]
+        
     wgtsList = []
     n = 0
     salto = 1/nportafolios
@@ -105,7 +135,7 @@ def simulacion_3_curvas(nportafolios):
     vols = []
     for n in cols:
         
-        var = variaciones()
+        var = variaciones_csv()
         var.drop(var.columns[n],axis=1,inplace=True)
         
         curvar = []
@@ -125,4 +155,5 @@ def simulacion_3_curvas(nportafolios):
     graficar_3_curvas(rends, vols)
     
 if __name__ == '__main__':
-    simulacion_3_curvas(100)
+    f = r'Insumos\sectoresmx.csv'
+    graficar_retorno_volatilidad(f)
